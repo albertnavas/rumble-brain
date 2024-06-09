8c27484af81d05b489d34f1a56df27f4f93be03b
Albert Navas <albertnm76@gmail.com>
2024-06-09 18:02:13 +0200

    chore: fix sveltkit 2 top-level promises are no longer awaited

--- client/src/routes/(admin)/juego/editar/+page.ts
+++ client/src/routes/(admin)/juego/editar/+page.ts
@@ -2,8 +2,8 @@ import type { PageLoad } from './$types'
 
 import { getGame } from '$lib/infrastructure/http/admin/adminHTTP'
 
-export const load = ((params) => {
+export const load = (async (params) => {
   const gameId = params.url.searchParams.get('gameId')
   if (!gameId) throw new Error('gameId is required')
-  return getGame(gameId)
+  return await getGame(gameId)
 }) satisfies PageLoad
--- client/src/routes/(admin)/juego/play/+page.ts
+++ client/src/routes/(admin)/juego/play/+page.ts
@@ -2,12 +2,12 @@ import type { PageLoad } from './$types'
 
 import { getGame, getGamePlayers } from '$lib/infrastructure/http/admin/adminHTTP'
 
-export const load = ((params) => {
+export const load = (async (params) => {
   const gameId = params.url.searchParams.get('gameId')
   if (!gameId) throw new Error('gameId is required')
 
-  const game = getGame(gameId)
-  const gamePlayers = getGamePlayers(gameId)
+  const game = await getGame(gameId)
+  const gamePlayers = await getGamePlayers(gameId)
 
   return { game, gamePlayers }
 }) satisfies PageLoad
